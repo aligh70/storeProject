3a121bfce34d60ae001506b82ab3f60173ea32d2
Ali Ghaffari <aligh71@yahoo.com>
2024-02-04 22:16:09 +0330

    implement the reducer function

--- src/componenets/Card.jsx
+++ src/componenets/Card.jsx
@@ -8,9 +8,10 @@ import { useCart } from "../context/CartContext";
 function Card({ data }) {
   const { id, title, image, price } = data;
   const [state, dispatch] = useCart();
+  console.log(state);
 
   const clickHandler = () => {
-    dispatch({ type: "add", payload: data });
+    dispatch({ type: "ADD_ITEM", payload: data });
   };
   return (
     <div className={styles.card}>
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,9 +1,29 @@
 import { createContext, useContext, useReducer } from "react";
+import { sumProducts } from "../helper/helper";
 
-const initialState = {};
+const initialState = {
+  selectedItems: [],
+  itemsCounter: 0,
+  total: 0, //price
+  checkOut: false,
+};
 
 const reducer = (state, action) => {
   console.log(action);
+  switch (action.type) {
+    case "ADD_ITEM":
+      if (!state.selectedItems.find((item) => item.id === action.payload.id)) {
+        state.selectedItems.push({ ...action.payload, quantity: 1 });
+      }
+      return {
+        ...state,
+        ...sumProducts(state.selectedItems),
+        checkOut: false,
+      };
+
+    default:
+      throw new Error("Invalid Action!");
+  }
 };
 
 const CartContext = createContext();
