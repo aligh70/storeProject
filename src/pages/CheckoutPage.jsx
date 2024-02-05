dc1aaba485dec7356f5969729a7f63bf213fd0ab
Ali Ghaffari <aligh71@yahoo.com>
2024-02-05 22:35:44 +0330

    check out the sidebar related to the shopping cart

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,14 +1,26 @@
 import BasketCard from "../componenets/BasketCard";
+import BasketSidebar from "../componenets/BasketSidebar";
 import { useCart } from "../context/CartContext";
+
+import styles from "./CheckoutPage.module.css";
 
 function CheckoutPage() {
   const [state, dispatch] = useCart();
 
   const clickHandler = (type, payload) => dispatch({ type, payload });
 
+  if (!state.itemsCounter) {
+    return (
+      <div className={styles.container}>
+        <p>Empty</p>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <div>
+    <div className={styles.container}>
+      <BasketSidebar state={state} clickHandler={clickHandler} />
+      <div className={styles.products}>
         {state.selectedItems.map((product) => (
           <BasketCard
             key={product.id}
